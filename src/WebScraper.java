18fc0fc2b5f7f8b9cc58a1fa421d29834af13ab9
taele <tlee95@illinois.edu>
2018-02-06 18:47:19 -0600

    Searches hamlet for the word "prince"

--- src/WebScraper.java
+++ src/WebScraper.java
@@ -9,6 +9,18 @@ public class WebScraper {
      * @param url url to retrieve contents from
      * @return the contents from the url as a string, or an empty string on error
      */
+    public static int wordCount(final String text, final String keyword) {
+        String lower = text.toLowerCase();
+        String[] newText = lower.split("\\n\\s+");
+        int count = 0;
+        for (int i = 0; i < newText.length; i++) {
+            if (newText[i].contains(keyword)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
     public static String urlToString(final String url) {
         Scanner urlScanner;
         try {
@@ -21,14 +33,8 @@ public class WebScraper {
         return contents;
     }
 
-    public static void main(String[] unused){
-
-
-
-
-
-
-
-
+    public static void main(String[] unused) {
+        String hamlet = WebScraper.urlToString("http://erdani.com/tdpl/hamlet.txt");
+        System.out.println(wordCount(hamlet, "prince"));
     }
 }
